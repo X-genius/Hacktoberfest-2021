da334dde726784f875528bbb1fe07031e7f9d4c8
Aman <55349238+X-genius@users.noreply.github.com>
2021-10-19 00:50:09 +0530

    Update Check Cycle Solution with comments

--- Ayu10x/LeetCode_Q_141.java
+++ Ayu10x/LeetCode_Q_141.java
@@ -9,14 +9,31 @@ class ListNode1 {
 
 public class LeetCode_Q_141 {
     public boolean hasCycle(ListNode1 head) {
-        if(head == null || head.next == null) return false;
-        ListNode1 p = head, q = head.next;
-        while(p != null && q != null){
-            if(p == q) return true;
-            p = p.next;
-            q = q.next;
-            if(q != null) q = q.next;
+       //If head is null return false because there is no node to create cycle.
+       //edge case
+       if(head == null)
+       {
+           return false;
+       }
+        
+       //Create two nodes slow and fast pointer which point head
+       ListNode1 slow = head;
+       ListNode1 fast = head;
+        
+        //running a loop with three condition
+        while(slow != null && fast != null && fast.next != null)
+        {
+            //Increase slow one time and fast two time.
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            //If slow and fast meet same node so this linked list create cycle.
+            if(slow == fast)
+            {
+                return true;
+            }
         }
-        return false;
+        //if no return false.
+        return false;   
     }
 }
